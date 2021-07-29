cc26ecdbcb08f6b22bddca0ed0e74d850a33b80b
pavel <zzzzfogzzzz@gmail.com>
2021-07-29 21:03:52 +0600

    hw_4.1

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,4 +8,23 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class Controller {
+    @FXML
+    public TextField textField;
+    @FXML
+    public TextArea textArea;
+
+    @FXML
+    public void sendMessage(ActionEvent actionEvent) {
+        textArea.appendText(textField.getText() + "\n");
+        textField.clear();
+        textField.requestFocus();
+    }
+
+    @FXML
+    public void close(ActionEvent actionEvent) {
+        Platform.runLater(() -> {
+            Stage stage = (Stage) textArea.getScene().getWindow();
+            stage.close();
+        });
+    }
 }
